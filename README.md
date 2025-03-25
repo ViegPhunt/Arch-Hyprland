df1ae174d461b6b69f8033b8b53f6be03914ce95
Viet Phung <phungquocviet0608@gmail.com>
2025-03-25 11:22:26 +0700

    First commit, up config file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Arch-Hyprland
+# My Arch-Hyprland Install Script
+This script automates install and setup my Arch Hyprland
+
+## Necessary
+> [!IMPORTANT]
+> Install a backup tool and create a system backup before using this script.
+
+> [!NOTE]
+> This script does not include package uninstallation, as some packages may have already been installed on your system by default. Creating a package uninstallation script could potentially affect your previous system.
+
+
+## Installation
+Using this script to install Hyprland on an Arch-based system
+```
+sudo pacman -Syu --noconfirm
+bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/Arch-Hyprland/main/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+echo "WELCOME! Now we will install and setup Hyprland on an Arch-based system"
+echo "Create by Phunt_Vieg_"
+
+sudo pacman -Syu --noconfirm
+
+cd ~
+
+echo "Install yay"
+sudo pacman -S --noconfirm --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ~
+rm -rf ~/yay
+
+echo "Install package"
+sudo pacman -S --noconfirm hyprland ghostty stow nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl bluez bluez-utils blueman discord
+yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin
+
+echo "PIP Install"
+pip install Pillow
+
+echo "Enable bluetooth"
+sudo systemctl enable --now bluetooth
+
+echo "Install JetBrainsMono Nerd Font"
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+fc-cache -fv
+cd ~
+
+echo "Stow dotfiles"
+git clone https://github.com/ViegPhunt/Arch-Hyprland.git ~/Arch-Hyprland
+mkdir -p ~/dotfiles && mv ~/Arch-Hyprland/dotfiles/* ~/dotfiles/
+rm -rf ~/Arch-Hyprland
+cd ~/dotfiles
+stow ghostty hypr rofi swww wallpaper waybar wlogout
+cd ~
+
+echo "Have a great time with Hyprland!!!"
