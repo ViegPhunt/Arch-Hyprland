0bdcb2aab6740e2037634f4071448b11fdd1ba72
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-03-27 02:36:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,20 +7,12 @@ sudo pacman -Syu --noconfirm
 
 cd ~
 
-echo "Install yay"
-sudo pacman -S --noconfirm --needed base-devel git
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ~
-rm -rf ~/yay
-
-echo "Install package"
-sudo pacman -S --noconfirm hyprland ghostty stow nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl network-manager-applet bluez bluez-utils blueman fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo discord
-yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin
-
 echo "Setup Terminal"
 bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/auto-setup-LT/main/arch.sh)"
+
+echo "Install package"
+sudo pacman -S --noconfirm hyprland ghostty nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl network-manager-applet bluez bluez-utils blueman fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo discord
+yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin
 
 echo "PIP Install"
 pip install Pillow
