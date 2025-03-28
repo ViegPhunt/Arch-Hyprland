abe7defc0c5482d8c086ba8974129b4f975fce19
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-03-29 01:21:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/auto-setup-LT/m
 
 echo "Install package"
 sudo pacman -S --noconfirm hyprland ghostty nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl network-manager-applet bluez bluez-utils blueman fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo discord pipewire wireplumber grim slurp obs-studio
-yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin xdg-desktop-portal-hyprland
+yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin xdg-desktop-portal-hyprland rofi-greenclip
 
 echo "PIP Install"
 pip install Pillow
