c0b01ebf379f7e101d0ccf8136b5737cce5f687c
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-03-29 16:13:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "Setup Terminal"
 bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/auto-setup-LT/main/arch.sh)"
 
 echo "Install package"
-sudo pacman -S --noconfirm hyprland ghostty nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl network-manager-applet bluez bluez-utils blueman fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo discord pipewire wireplumber grim slurp gnome-text-editor evince cheese sddm obs-studio
+sudo pacman -S --noconfirm hyprland ghostty nemo swww waybar rofi rofi-emoji hyprlock swaync brightnessctl network-manager-applet bluez bluez-utils blueman fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo discord pipewire wireplumber grim slurp gnome-text-editor okular cheese sddm obs-studio
 yay -S --noconfirm wlogout hyprshot brave-bin spotify cava visual-studio-code-bin xdg-desktop-portal-hyprland rofi-greenclip
 
 echo "PIP Install"
