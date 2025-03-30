35af8c745128d74a17a40a5611a84a193d53234c
Viet Phung <phungquocviet0608@gmail.com>
2025-03-30 20:16:28 +0700

    Add installpkg file

---
+++ dotfiles/hypr/.config/hypr/scripts/installpkg.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+pacman_packages=(
+    hyprland
+    ghostty
+    nemo
+    swww
+    waybar
+    rofi
+    rofi-emoji
+    hyprlock
+    swaync
+    brightnessctl
+    network-manager-applet
+    bluez
+    bluez-utils
+    blueman
+    fcitx5
+    fcitx5-gtk
+    fcitx5-qt
+    fcitx5-configtool
+    fcitx5-bamboo
+    discord
+    pipewire
+    wireplumber
+    grim
+    slurp
+    gnome-text-editor
+    okular
+    cheese
+    sddm
+    obs-studio
+)
+
+aur_packages=(
+    wlogout
+    hyprshot
+    brave-bin
+    spotify
+    cava
+    visual-studio-code-bin
+    xdg-desktop-portal-hyprland
+    rofi-greenclip
+)
+
+sudo pacman -S --noconfirm "${pacman_packages[@]}"
+yay -S --noconfirm "${aur_packages[@]}"
