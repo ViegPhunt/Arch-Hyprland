a8bba98e9af4114419e2867439a9a978c5b8bf87
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-04-18 01:33:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,13 @@ echo "Enable networkmanager"
 sudo systemctl enable --now NetworkManager
 
 echo "Install JetBrainsMono Nerd Font"
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
+fc-cache -fv
+cd ~
 
 echo "Stow dotfiles"
 git clone https://github.com/ViegPhunt/Arch-Hyprland.git ~/Arch-Hyprland
