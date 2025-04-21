aa51faf39a8fad422672d8f16f7bc81b04c1b01f
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-04-22 01:34:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 echo "WELCOME! Now we will install and setup Hyprland on an Arch-based system"
 echo "Create by Phunt_Vieg_"
 
+cd ~
+
 sudo pacman -Syu --noconfirm
 
-cd ~
+echo "Install Fonts"
+bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/Arch-Hyprland/refs/heads/main/dotfiles/hypr/.config/hypr/scripts/installfonts.sh)"
 
 echo "Setup Terminal"
 bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/auto-setup-LT/main/arch.sh)"
@@ -22,17 +25,12 @@ sudo systemctl enable --now bluetooth
 echo "Enable networkmanager"
 sudo systemctl enable --now NetworkManager
 
-echo "Install Fonts"
-bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/Arch-Hyprland/refs/heads/main/dotfiles/hypr/.config/hypr/scripts/installfonts.sh)"
-
 echo "Set Cursor"
 bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/Arch-Hyprland/refs/heads/main/dotfiles/hypr/.config/hypr/scripts/setcursor.sh)"
 
 echo "Stow dotfiles"
-git clone https://github.com/ViegPhunt/Arch-Hyprland.git ~/Arch-Hyprland
 mkdir -p ~/dotfiles && mv ~/Arch-Hyprland/dotfiles/* ~/dotfiles/
-rm -rf ~/Arch-Hyprland
-git clone https://github.com/ViegPhunt/Wallpaper-Collection.git
+git clone https://github.com/ViegPhunt/Wallpaper-Collection.git ~/Wallpaper-Collection
 mkdir -p ~/dotfiles/wallpaper/.config/wallpaper && mv ~/Wallpaper-Collection/Wallpapers/* ~/dotfiles/wallpaper/.config/wallpaper
 rm -rf ~/Wallpaper-Collection
 cd ~/dotfiles
