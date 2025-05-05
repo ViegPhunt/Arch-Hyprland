82119629e78653a8a84e17312368c9d1828caa95
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2025-05-05 22:26:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,55 @@
 #!/usr/bin/bash
 
-echo "WELCOME! Now we will install and setup Hyprland on an Arch-based system"
-echo "Create by Phunt_Vieg_"
+clear
+
+echo "==> WELCOME! Now we will install and setup Hyprland on an Arch-based system"
+echo "==> Create by Phunt_Vieg_"
 
 cd ~
 
+echo "==> Updating system packages..."
 sudo pacman -Syu --noconfirm
 
-echo "Setup Terminal"
+echo "==> Setup Terminal"
 bash -c "$(curl -fSL https://raw.githubusercontent.com/ViegPhunt/auto-setup-LT/main/arch.sh)"
 
-echo "Make executable"
+echo "==> Make executable"
 sudo chmod +x ~/dotfiles/.config/hypr/scripts/*
 
-echo "Download wallpaper"
+echo "==> Download wallpaper"
 git clone --depth 1 https://github.com/ViegPhunt/Wallpaper-Collection.git ~/Wallpaper-Collection
-mkdir -p ~/Pictures/Wallpapers && mv ~/Wallpaper-Collection/Wallpapers/* ~/Pictures/Wallpapers
+mkdir -p ~/Pictures/Wallpapers
+mv ~/Wallpaper-Collection/Wallpapers/* ~/Pictures/Wallpapers
 rm -rf ~/Wallpaper-Collection
 
-echo "Install package"
+echo "==> Install package"
 ~/dotfiles/.config/hypr/scripts/installpkg.sh
 
-echo "PIP Install"
-pip install Pillow
+echo "==> PIP Install"
+pip3 install Pillow
 
-echo "Enable bluetooth"
+echo "==> Enable bluetooth"
 sudo systemctl enable --now bluetooth
 
-echo "Enable networkmanager"
+echo "==> Enable networkmanager"
 sudo systemctl enable --now NetworkManager
 
-echo "Set Cursor"
+echo "==> Set Cursor"
 ~/dotfiles/.config/hypr/scripts/setcursor.sh
 
-echo "Stow dotfiles"
+echo "==> Stow dotfiles"
 cd ~/dotfiles
 stow -t ~ .
 cd ~
 
-echo "Setup SDDM"
+echo "==> Setup SDDM"
 sudo systemctl enable sddm
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf
 
+echo
+echo "Hyprland setup is complete!"
+echo
+echo "It is recommended to reboot your system to apply all changes."
+echo
 echo "Have a great time with Hyprland!!!"
+echo
